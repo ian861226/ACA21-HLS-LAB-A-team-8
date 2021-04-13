971c4adee6fa17c339f1fbd4e0ecf91d8d49868c
ian861226 <71866752+ian861226@users.noreply.github.com>
2021-04-13 11:42:33 +0800

    Update shifter.h

--- shifter/src/shifter.h
+++ shifter/src/shifter.h
@@ -5,7 +5,7 @@
 #define NUM_BITS 8
 #define CTRL_BITS 4
 
-void shifter(ap_uint<3> mode, ap_uint<NUM_BITS> din, ap_uint<CTRL_BITS> s, ap_uint<NUM_BITS>* dout);
+void shifter(ap_uint<4> mode, ap_uint<NUM_BITS> din, ap_uint<CTRL_BITS> s, ap_uint<NUM_BITS>* dout);
 ap_int<NUM_BITS> barrel_shift_al(ap_int<NUM_BITS> din, ap_uint<CTRL_BITS> s);
 ap_int<NUM_BITS> barrel_shift_ar(ap_int<NUM_BITS> din, ap_uint<CTRL_BITS> s);
 ap_uint<NUM_BITS> barrel_shift_rr(ap_uint<NUM_BITS> din, ap_uint<CTRL_BITS> s);
@@ -14,5 +14,6 @@ ap_uint<NUM_BITS> barrel_shift_lr(ap_uint<NUM_BITS> din, ap_uint<CTRL_BITS> s);
 ap_uint<NUM_BITS> barrel_shift_lr_const(ap_uint<NUM_BITS> din, ap_uint<CTRL_BITS> s);
 ap_uint<NUM_BITS> shift_mask_dynamic(ap_uint<NUM_BITS> din);
 ap_uint<NUM_BITS> shift_mask_static(ap_uint<NUM_BITS> din);
+ap_uint<NUM_BITS> bi_shift(ap_uint<NUM_BITS> din, ap_int<CTRL_BITS> s);
 
 #endif
